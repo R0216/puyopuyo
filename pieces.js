40f9bcc54e7d060b975498084cfee95fe1228fae
R0216 <s1f102500247@iniad.org>
2026-02-21 01:31:35 +0900

    ぷよ一つ分を生成する関数の作成

---
+++ pieces.js
@@ -0,0 +1,8 @@
+export const COLORS = {
+    1: "red",
+    2: "green",
+    3: "blue",
+    4: "yellow",
+    5: "purple",
+}
+    
--- script.js
+++ script.js
@@ -1,4 +1,25 @@
+import { COLORS } from "./pieces.js";
 const canvas = document.getElementById("canvas");
 const context = canvas.getContext("2d");
 
 context.scale(60, 60);
+const board = Array.from({ length: 12 }, () => Array(6).fill(0));
+
+const player = {
+    pos: {x: 2, y: 0},
+    color: Math.floor(Math.random() * 5) + 1,
+}
+
+function draw() {
+    context.fillStyle = "balck";
+    context.fillRect(0, 0, canvas.width, canvas.height);
+
+    drawPuyo(player.pos.x, player.pos.y, player.color);
+}
+
+function drawPuyo(x, y, colorIndex) {
+    context.fillStyle = COLORS[colorIndex];
+    context.fillRect(x, y, 1, 1);
+}
+
+draw();

ea00358ed77c8b2f87fcc7a1ad4a565821b283c5
R0216 <s1f102500247@iniad.org>
2026-02-23 17:57:09 +0900

    衝突判定の追加

--- script.js
+++ script.js
@@ -1,25 +1,84 @@
-import { COLORS } from "./pieces.js";
+import { COLORS } from "./jsフォルダ/pieces.js";
 const canvas = document.getElementById("canvas");
 const context = canvas.getContext("2d");
 
 context.scale(60, 60);
 const board = Array.from({ length: 12 }, () => Array(6).fill(0));
-
+console.table(board);
 const player = {
     pos: {x: 2, y: 0},
     color: Math.floor(Math.random() * 5) + 1,
+    subColor: Math.floor(Math.random() * 5) + 1,
+    subPos: {x: 0, y: -1},
+}
+
+function collide(board, player) {
+    const puyos = [
+        {x: player.pos.x, y: player.pos.y},
+        {x: player.pos.x + player.subPos.x, y: player.pos.y + player.subPos.y}
+    ];
+
+    for (const p of puyos) {
+        if (p.x > 0 || p.x >= 6 || p.y >= 12 || (p.y >= 0 && board[p.y[p.x] !== 0])){
+            return true
+        }
+    }
+    return false;   
+}
+
+function merge(board, player) {
+    board[player.pos.y][player.poa.x] = player.color;
+    baord[player.pos.y + player.subPos.y][player.pos.x + player.subPos.x] = player.subColor;
+}
+
+function playerReset() {
+    player.pos.y = 0;
+    player.pos.x = 2;
+    player.color = Math.floor(Math.random() * 5) + 1;
+    player.subColor = Math.floor(Math.random() * 5) + 1;
 }
 
 function draw() {
-    context.fillStyle = "balck";
+    context.fillStyle = "black";
     context.fillRect(0, 0, canvas.width, canvas.height);
 
+    board.forEach((row, y) => {
+        row.forEach((value, x) => {
+            if(value > 0){
+                drawPuyo(x, y, value);
+            }
+        });
+    });
     drawPuyo(player.pos.x, player.pos.y, player.color);
+    drawPuyo(player.pos.x + player.subPos.x, player.pos.y + player.subPos.y, player.subColor);
+    requestAnimationFrame(draw);
 }
 
 function drawPuyo(x, y, colorIndex) {
     context.fillStyle = COLORS[colorIndex];
-    context.fillRect(x, y, 1, 1);
+    context.beginPath();
+    context.arc(x + 0.5, y + 0.5, 0.45, 0, Math.PI * 2);
+    context.fill();
+
+    context.strokeStyle = "white";
+    context.lineWidth = 0.05;
+    context.stroke();
 }
+
+window.addEventListener("keydown", (event) => {
+    if(event.key === "ArrowDown") {
+        if(player.pos.y < 11) {
+            player.pos.y++;
+        } else {
+            console.log("着地")
+        }
+        
+        console.log(player.pos.y);
+    } else if(event.key === "ArrowRight") {
+        player.pos.x++;
+    } else if(event.key === "ArrowLeft") {
+        player.pos.x--;
+    }
+})
 
 draw();
